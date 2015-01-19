05d69b9b1500c584e74056032fefa039cca6bf96
Alexandre Bury <alexandre.bury@gmail.com>
2015-01-19 15:32:06 -0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func readToken() string {
+	body, err := ioutil.ReadFile("token")
+	if err != nil {
+		log.Println("Could not read file \"token\". Does this file exist?")
+		log.Fatal(err)
+	}
+
+	return strings.Trim(string(body), " \n")
+}
+
+func main() {
+	token := readToken()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.ParseFiles("index.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		t.Execute(w, struct {
+			Token string
+		}{
+			token,
+		})
+	})
+	http.ListenAndServe(":8888", nil)
+}
